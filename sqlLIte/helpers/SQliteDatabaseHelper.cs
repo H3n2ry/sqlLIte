a8525cf9c110b84b5b8cacfc674c27cd3be56c07
Henry de Araujo Fernandes <H3n2ry.af@gmail.com>
2025-02-28 00:22:30 -0300

    Update SQliteDatabaseHelper.cs

--- sqlLIte/helpers/SQliteDatabaseHelper.cs
+++ sqlLIte/helpers/SQliteDatabaseHelper.cs
@@ -1,12 +1,51 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using SQLite;
+using sqlLIte.models;
 
 namespace sqlLIte.helpers
 {
-    class SQliteDatabaseHelper
+    public class SQliteDatabaseHelper
+{
+    readonly SQLiteAsyncConnection _conn;
+
+    public SQliteDatabaseHelper(string caminho)
     {
+        _conn = new SQLiteAsyncConnection(caminho);
+        _conn.CreateTableAsync<Produto>().Wait();
     }
+
+   
+        public  Task <int> Insert(Produto p) 
+        {
+            return _conn.InsertAsync(p);
+        }
+
+        public Task<List<Produto>> Update(Produto p) 
+        { 
+            string sql = "UPDATE Produto SET Descricao  = ?, Quantidade  = ?,  Preco = ? WHERE Id = ?";
+
+            return _conn.QueryAsync<Produto>(
+                sql, p.Descricao, p.Quantidade, p.Preco, p.Id
+            );
+        }
+
+        public Task<int> Delete(int id)
+        { 
+          return  _conn.Table<Produto>().DeleteAsync(i => i.Id == id);
+    }
+
+        public Task<List<Produto>> getall() 
+        { 
+            return _conn.Table<Produto>().ToListAsync();
+    }
+
+        public Task<List<Produto>> search(string q) 
+        {
+                string sql = "SELECT  * Produto  WHERE  Descricao LIKE '%" + q + "%'";
+
+                return _conn.QueryAsync<Produto>(sql);
+        }
+
+
+
+}
 }
