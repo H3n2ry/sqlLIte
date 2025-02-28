30546ada2b84e7ce23646e5796bdc2ba2d4358d0
Henry de Araujo Fernandes <H3n2ry.af@gmail.com>
2025-02-28 00:25:39 -0300

    Update App.xaml.cs

--- sqlLIte/App.xaml.cs
+++ sqlLIte/App.xaml.cs
@@ -4,12 +4,9 @@
     {
         public App()
         {
-            InitializeComponent();
-        }
+                InitializeComponent();
 
-        protected override Window CreateWindow(IActivationState? activationState)
-        {
-            return new Window(new AppShell());
+                MainPage = new NavigationPage(new views.ListaProduto());
         }
     }
 }
